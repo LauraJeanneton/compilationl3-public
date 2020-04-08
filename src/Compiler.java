26caa649a9effb37ddc37d022f15e1d148c33edc
LauraJeanneton <laura.jeanneton@etu.univ-amu.fr>
2020-04-08 17:55:25 +0200

    Changement Compiler.java

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,7 +1,11 @@
 import c3a.C3a;
+import c3a.C3aEval;
 import fg.Fg;
+import fg.FgSolution;
+import ig.Ig;
 import nasm.Nasm;
 import sa.Sa2Xml;
+import sa.SaEval;
 import sa.SaNode;
 import sc.lexer.Lexer;
 import sc.node.Start;
@@ -27,57 +31,81 @@ public class Compiler {
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".l")) {
                 if(!listOfFiles[i].getName().contains("tab1"))
-                fileNames.add(listOfFiles[i].getAbsolutePath());
+                    fileNames.add(listOfFiles[i].getAbsolutePath());
             }
         }
+
         try {
             for (String file : fileNames) {
-                //if (file.equals("C:\\Users\\Laura\\Desktop\\compilationl3-public\\test\\input\\add1.l")) {
+               // if(file.contains("affect")){
                 br = new PushbackReader(new FileReader(file));
                 baseName = removeSuffix(file, ".l");
-
                 try {
-                  //  System.out.println(file);
                     Parser p = new Parser(new Lexer(br));
+                //    System.out.print("[BUILD SC] ");
                     Start tree = p.parse();
-                    //System.out.println("[SC]");
+                //    System.out.println("[PRINT SC]");
                     tree.apply(new Sc2Xml(baseName));
 
-                    //System.out.println("[SA]");
+               //     System.out.print("[BUILD SA] ");
                     Sc2sa sc2sa = new Sc2sa();
                     tree.apply(sc2sa);
                     SaNode saRoot = sc2sa.getRoot();
+               //     System.out.println("[PRINT SA]");
                     new Sa2Xml(saRoot, baseName);
-                    if(!compareTest(baseName,".sa"))System.out.println("False sa: " +baseName);
-                    //System.out.println("Fin de l'arbre ");
+                    if(!compareTest(baseName,".sa")) System.out.println("False sa : " + baseName);
 
-                    //System.out.println("[TABLE SYMBOLES]");
+                 //   System.out.print("[BUILD TS] ");
                     Ts table = new Sa2ts(saRoot).getTableGlobale();
+                 //   System.out.println("[PRINT TS]");
                     table.afficheTout(baseName);
-                    if(!compareTest(baseName,".ts"))System.out.println("False ts: " +baseName);
-                    //System.out.println("[C3A]");
-	                C3a c3a = new Sa2c3a(saRoot, table).getC3a();
-	                c3a.affiche(baseName);
+                    if(!compareTest(baseName,".ts")) System.out.println("False ts : " + baseName);
+
+
+//                    SaEval saEval = new SaEval(saRoot, table);
+//                    saEval.affiche(baseName);
+                  //  if(!compareTest(baseName,".saout")) System.out.println("False saout : " + baseName);
+
+
+                    C3a c3a = new Sa2c3a(saRoot, table).getC3a();
+                    c3a.affiche(baseName);
                     if(!compareTest(baseName,".c3a"))System.out.println("False c3a: " +baseName);
 
-	                Nasm nasm = new C3a2nasm(c3a, table).getNasm();
-	                 nasm.affichePre(baseName);
-                    //if(!compareTest(baseName,".pre-nasm"))System.out.println("False pre-nasm: " +baseName);
-	                nasm.affiche(baseName);
-	                //if(!compareTest(baseName,".nasm"))System.out.println("False nasm: " +baseName);
+                    C3aEval c3aEval = new C3aEval(c3a, table);
+                    c3aEval.affiche(baseName);
+                    if(!compareTest(baseName,".c3aout"))System.out.println("False c3aout: " +baseName);
 
-            	    Fg fg = new Fg(nasm);
-	                fg.affiche(baseName);
-                    if(!compareTest(baseName,".fg"))System.out.println("False fg: " +baseName);
-	    /*
-	    System.out.println("[FLOW GRAPH SOLVE]");
-	    FgSolution fgSolution = new FgSolution(nasm, fg);
-	    fgSolution.affiche(baseName);*/
+
+                    //System.out.print("[BUILD PRE NASM] ");
+                    Nasm nasm = new C3a2nasm(c3a, table).getNasm();
+                    //System.out.println("[PRINT PRE NASM] ");
+                    nasm.affichePre(baseName);
+                    //if(!compareTest(baseName,".pre-nasm"))System.out.println("False prenasm: " +baseName);
+
+             //       System.out.print("[BUILD FG] ");
+                    Fg fg = new Fg(nasm);
+                 //   System.out.print("[PRINT FG] ");
+                    fg.affiche(baseName);
+                   // if(!compareTest(baseName,".fg"))System.out.println("False fg: " +baseName);
+//
+//
+//                    System.out.println("[SOLVE FG]");
+//                    FgSolution fgSolution = new FgSolution(nasm, fg);
+//                    fgSolution.affiche(baseName);
+//
+//                    System.out.print("[BUILD IG] ");
+//                    Ig ig = new Ig(fgSolution);
+//                    System.out.print("[PRINT IG] ");
+//                    ig.affiche(baseName);
+//                    System.out.println("[ALLOCATE REGISTERS]");
+//                    ig.allocateRegisters();
+//                    System.out.println("[PRINT NASM]");
+//                    nasm.affiche(baseName);
+
                 } catch (Exception e) {
-                    System.out.println("Catch");
-                    System.out.println(e.getMessage());
+                    System.out.println("ERROR : " + baseName);
                 }
-                //  } ////
+               //   } ////
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -112,7 +140,7 @@ public class Compiler {
             int toSub = 54+ (extension.length()-1) ;
             if (extension.equals(".pre-nasm")) toSub= toSub-1;
             String newfile=file.substring(toSub);
-           // System.out.println(newBaseName + " " + newfile);
+            // System.out.println(newBaseName + " " + newfile);
             if(newfile.equals(newBaseName)) {
                 same = compareFiles(file,baseName,extension);
             }
